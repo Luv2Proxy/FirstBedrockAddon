f989234636f2967d05e3ad3befb26de0c25ae605
Luv2Proxy <abohra1@outlook.com>
2026-07-31 08:36:45 -0400

    Add world generation configuration

---
+++ behavior_pack/scripts/config/worldConfig.js
@@ -0,0 +1,16 @@
+export const WORLD_CONFIG = {
+  dimensionId: "sky_archipelago:archipelago",
+  regionSize: 512,
+  generationRadiusRegions: 1,
+  generationBudgetBlocksPerTick: 1200,
+  generationBudgetOperationsPerTick: 12,
+  islandDensity: 0.32,
+  minIslandRadius: 18,
+  maxIslandRadius: 110,
+  minIslandY: 80,
+  maxIslandY: 220,
+  verticalBandSize: 48,
+  oceanLevel: 64,
+  useVoidBelowOcean: true,
+  seedSalt: 0x51A2C9
+};
